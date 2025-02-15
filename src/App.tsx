9dc8757c34faab94ede88f55d1a36827f3de1ead
Charles Shen <cshencm@gmail.com>
2025-02-15 02:13:01 -0500

    print the current state of the data obj when the process btn is pressed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./index.css";
-import "./App.css";
 import {
   Button,
   Checkbox,
@@ -9,48 +8,91 @@ import {
   RadioChangeEvent,
   Switch,
 } from "antd";
-import { useState } from "react";
 import data from "./data";
+import { useState } from "react";
 
 function App() {
-  const [radioValue, setRadioValue] = useState(data.radioValue);
+  const [switchState, setSwitchState] = useState(data.switchState);
   const toolsOptions = ["Redux", "Lodash", "Ant Design", "Webpack", "Other"];
-  const [checkedTools, setCheckedTools] = useState(data.checkedTools);
 
-  const radioOnChange = (e: RadioChangeEvent) => {
-    setRadioValue(e.target.value);
+  const handleRadioChange = (e: RadioChangeEvent) => {
+    data.radioValue = e.target.value;
   };
+  const handlecCheckboxChange = (checkedTools: string[]) => {
+    data.checkedTools = checkedTools;
+  };
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    data.firstName = e.target.value;
+  };
+  const onFinish = () => {
+    console.log("Current state:", data);
+  };
+
+  // const onFinishFailed = (errorInfo: ValidateErrorEntity<FieldType>) => {
+  //   console.log("error:", errorInfo);
+  // };
+
   return (
-    <section className="container m-4 p-4">
+    <section className="container m-4 p-4 max-w-[400px]">
       <div className="flex flex-row justify-between items-center text-xl font-medium my-4">
         <p>Editable</p>
-        <Switch></Switch>
-      </div>
-      <Form className="flex flex-col gap-y-4">
-        <Input placeholder="First Name" />
-        <p>Are you proficient in ReactJS development?</p>
-        <Radio.Group
-          className="!flex flex-col"
-          value={radioValue}
-          onChange={radioOnChange}
-          options={[
-            { value: false, label: "No" },
-            { value: true, label: "Yes" },
-          ]}
+        <Switch
+          value={switchState}
+          onChange={(checked) => setSwitchState(checked)}
         />
+      </div>
+      <Form
+        className="flex flex-col gap-y-4"
+        onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        disabled={!switchState}
+      >
+        <Form.Item
+          name="firstName"
+          rules={[{ required: true, message: "Please input your first name!" }]}
+        >
+          <Input placeholder="First Name" onChange={handleTextChange} />
+        </Form.Item>
+
+        <p>Are you proficient in ReactJS development?</p>
+        <Form.Item
+          name="radioValue"
+          rules={[{ required: true, message: "Please choose one option!" }]}
+        >
+          <Radio.Group
+            className="!flex flex-col"
+            onChange={handleRadioChange}
+            options={[
+              { value: false, label: "No" },
+              { value: true, label: "Yes" },
+            ]}
+          />
+        </Form.Item>
+
         <div>
           <p>Which tools do you use?</p>
           <p>Please select all that apply.</p>
         </div>
-        <Checkbox.Group
-          className="!flex flex-col"
-          options={toolsOptions}
-          value={checkedTools}
-          onChange={setCheckedTools}
-        />
-        <Button type="primary" color="purple">
-          Process
-        </Button>
+        <Form.Item name="checkedTools">
+          <Checkbox.Group
+            className="!flex flex-col"
+            options={toolsOptions}
+            defaultValue={data.checkedTools}
+            onChange={handlecCheckboxChange}
+          />
+        </Form.Item>
+        <Form.Item className="flex justify-center *:w-[70%]">
+          <Button
+            type="primary"
+            color="purple"
+            htmlType="submit"
+            disabled={!switchState}
+            block
+          >
+            Process
+          </Button>
+        </Form.Item>
       </Form>
     </section>
   );
--- src/data.ts
+++ src/data.ts
@@ -1,7 +1,20 @@
-const data = {
-    switchState: true,
-    radioValue: true,
-    checkedTools: [],
-}
+// interface data {
+//     switchState: boolean,
+//     firstName: string,
+//     radioValue: boolean,
+//     checkedTools: string[],
+// }
+
+const data: {
+  switchState: boolean;
+  firstName: string;
+  radioValue: boolean;
+  checkedTools: string[];
+} = {
+  switchState: true,
+  firstName: "",
+  radioValue: false,
+  checkedTools: [],
+};
 
 export default data;
